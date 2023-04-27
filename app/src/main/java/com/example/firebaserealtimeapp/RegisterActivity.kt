1b1167159bf30fb8f7826814c0c58479eef4f41b
VentilationZ <elikibetael4@gmail.com>
2023-04-27 08:22:52 +0300

    fhg,jcsgfbhdc

--- app/src/main/java/com/example/firebaserealtimeapp/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaserealtimeapp/RegisterActivity.kt
@@ -1,11 +1,26 @@
 package com.example.firebaserealtimeapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var Btncreateacc : Button
+    private lateinit var EdtNameReg : TextView
+    private lateinit var EdtEmailReg : TextView
+    private lateinit var EdtPasswordReg : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        Btncreateacc = findViewById(R.id.btncreateacc)
+        EdtNameReg = findViewById(R.id.edtname)
+        EdtEmailReg = findViewById(R.id.edtemailreg)
+        EdtPasswordReg = findViewById(R.id.edtpasswordreg)
+
+        Btncreateacc.setOnClickListener {
+
+        }
     }
 }
